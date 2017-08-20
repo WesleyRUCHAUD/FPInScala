558987cdb57e921e3891c5c5398e2f2a97d792ca
Wesley Ruchaud <wesley.ruchaud@compte-nickel.fr>
2017-08-20 14:45:54 +0200

    Ajout des corrections des exercices 3, 4 et 5.

--- src/main/scala/chap2_getting_started/CurryCompose.scala
+++ src/main/scala/chap2_getting_started/CurryCompose.scala
@@ -11,4 +11,31 @@ case object CurryCompose {
   def compose[A,B,C](f: B => C, g: A => B): A => C =
     (a: A) => f(g(a))
 
+  /** Corrections from https://github.com/fpinscala/fpinscala/blob/master/answers/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala#L153. */
+  // Exercise 3: Implement `curry`.
+
+  // Note that `=>` associates to the right, so we could
+  // write the return type as `A => B => C`
+  def curryCor[A,B,C](f: (A, B) => C): A => (B => C) =
+    a => b => f(a, b)
+
+  // NB: The `Function2` trait has a `curried` method already
+
+  // Exercise 4: Implement `uncurry`
+  def uncurryCor[A,B,C](f: A => B => C): (A, B) => C =
+    (a, b) => f(a)(b)
+
+  /*
+  NB: There is a method on the `Function` object in the standard library,
+  `Function.uncurried` that you can use for uncurrying.
+  Note that we can go back and forth between the two forms. We can curry
+  and uncurry and the two forms are in some sense "the same". In FP jargon,
+  we say that they are _isomorphic_ ("iso" = same; "morphe" = shape, form),
+  a term we inherit from category theory.
+  */
+
+  // Exercise 5: Implement `compose`
+
+  def composeCor[A,B,C](f: B => C, g: A => B): A => C =
+    a => f(g(a))
 }
--- src/test/scala/chap2_getting_started/CurryComposeSpec.scala
+++ src/test/scala/chap2_getting_started/CurryComposeSpec.scala
@@ -40,9 +40,61 @@ class CurryComposeSpec extends WordSpec with Matchers {
     }
   }
 
+  "Method curryCor" must {
+    "return a function" when {
+      val addSquareAndN: Int => (Int => Int) = CurryCompose.curryCor(squareAndAddN)
+
+      "Function addSquareAndN" must {
+        "return an int value" when {
+          "is first input is 2" in {
+            val addSquare2AndN: Int => Int = addSquareAndN(2)
+
+            withClue("the result must be 7 if is second input is 3 but it was ") {
+              addSquare2AndN(3) should be(7)
+            }
+            withClue("the result must be 0 if is second input is -4 but it was ") {
+              addSquare2AndN(-4) should be(0)
+            }
+          }
+          "is first input is 3" in {
+            val addSquare3AndN: Int => Int = addSquareAndN(3)
+
+            withClue("the result must be 12 if is second input is 3 but it was ") {
+              addSquare3AndN(3) should be(12)
+            }
+            withClue("the result must be 5 if is second input is -4 but it was ") {
+              addSquare3AndN(-4) should be(5)
+            }
+          }
+        }
+      }
+    }
+  }
+
   "Method uncurry" must {
     "return a function" when {
       val addSquareAndSquare: (Int, Int) => Int = CurryCompose.uncurry(addTwoSquare)
+
+      "Function addSquareAndSquare" must {
+        "return an int value" when {
+          "is input are 2 and 3" in {
+            withClue("the result must be 13 but it was ") {
+              addSquareAndSquare(2, 3) should be(13)
+            }
+          }
+          "is input are 3 and 4" in {
+            withClue("the result must be 25 but it was ") {
+              addSquareAndSquare(3, 4) should be(25)
+            }
+          }
+        }
+      }
+    }
+  }
+
+  "Method uncurryCor" must {
+    "return a function" when {
+      val addSquareAndSquare: (Int, Int) => Int = CurryCompose.uncurryCor(addTwoSquare)
 
       "Function addSquareAndSquare" must {
         "return an int value" when {
@@ -87,6 +139,55 @@ class CurryComposeSpec extends WordSpec with Matchers {
     }
     "return a function who square and add two" when {
       val squareAndAddTwo: Int => Int = CurryCompose.compose(addTwo, square)
+
+      "Function squareAndAddTwo" must {
+        "return an int value" when {
+          "is input is -3" in {
+            withClue("the result must be 11 but it was ") {
+              squareAndAddTwo(-3) should be(11)
+            }
+          }
+          "is input is 0" in {
+            withClue("the result must be 2 but it was ") {
+              squareAndAddTwo(0) should be(2)
+            }
+          }
+          "is input is 5" in {
+            withClue("the result must be 27 but it was ") {
+              squareAndAddTwo(5) should be(27)
+            }
+          }
+        }
+      }
+    }
+  }
+
+  "Method composeCor" must {
+    "return a function who add two and square" when {
+      val addTwoAndSquare: Int => Int = CurryCompose.composeCor(square, addTwo)
+
+      "Function addTwoAndSquare" must {
+        "return an int value" when {
+          "is input is -3" in {
+            withClue("the result must be 1 but it was ") {
+              addTwoAndSquare(-3) should be(1)
+            }
+          }
+          "is input is 0" in {
+            withClue("the result must be 4 but it was ") {
+              addTwoAndSquare(0) should be(4)
+            }
+          }
+          "is input is 5" in {
+            withClue("the result must be 49 but it was ") {
+              addTwoAndSquare(5) should be(49)
+            }
+          }
+        }
+      }
+    }
+    "return a function who square and add two" when {
+      val squareAndAddTwo: Int => Int = CurryCompose.composeCor(addTwo, square)
 
       "Function squareAndAddTwo" must {
         "return an int value" when {
